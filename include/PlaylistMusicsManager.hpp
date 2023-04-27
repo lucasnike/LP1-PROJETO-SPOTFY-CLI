1e579f2c3ed105ef56a6beb927b0fbf2dfde00e8
Lucas Silva <lucas4crital@gmail.com>
2023-04-27 19:56:32 -0300

    Gerenciador de músicas na playlist selecionada

---
+++ include/PlaylistMusicsManager.hpp
@@ -0,0 +1,8 @@
+#include "Playlist.hpp"
+
+#ifndef PMN_HPP
+#define PMN_HPP
+
+void addMusic(Playlist *);
+
+#endif
---
+++ src/PlaylistMusicsManager.cpp
@@ -0,0 +1,11 @@
+#include "PlaylistMusicsManager.hpp"
+#include "Playlist.hpp"
+#include <iostream>
+#include "MusicManager.hpp"
+
+using namespace std;
+
+void addMusic(Playlist *playlist, List<Music*> musics)
+{
+    listAllMusic(musics);
+}
